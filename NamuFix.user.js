e87f6958e245e5e629935ad109b55629731bb737
Korean12 <spinpo12@naver.com>
2015-05-26 16:26:21 +0900

    addfile
    새로운 컬러픽커

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -537,7 +537,6 @@ if(document.querySelector("textarea[name=content]")!=null&&(/https?:\/\/[^\.]*\.
     }
     txtarea.value=txtarea.value.substring(0,s)+result+txtarea.value.substring(e);
   }
-  
   buttons.id="EditInterfaceButtons";
   editstatus.id="EditInterfaceStatus";
   // 서식 버튼
@@ -565,3 +564,4 @@ if(document.querySelector("textarea[name=content]")!=null&&(/https?:\/\/[^\.]*\.
   txtarea.parentNode.insertBefore(buttons,txtarea);
   txtarea.parentNode.insertBefore(editstatus,txtarea);
 }
+//나무페이지 설정 애드온
