fcca4602b433aed6ed04eaab105d7d1035643d49
LiteHell <asdf1234d@gmail.com>
2015-05-28 21:39:23 +0900

    Added Include function

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -7,9 +7,22 @@
 // @version     3.15
 // @namespace   http://litehell.info/
 // @downloadURL https://raw.githubusercontent.com/LiteHell/NamuFix/dev/NamuFix.user.js
+// @require     https://raw.githubusercontent.com/LiteHell/NamuFix/dev/static/FlexiColorPicker.js
 // @grant       GM_addStyle
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @run-at      document-end
 // ==/UserScript==
+
+function Include(path){
+  GM_xmlhttpRequest({
+    method: "GET",
+    url: "https://raw.githubusercontent.com/LiteHell/NamuFix/dev/src/"+path,
+    onload:function(response){
+      eval(response.responseText);
+    }
+  });
+}
+
+Include('ModifyEditor.js');
