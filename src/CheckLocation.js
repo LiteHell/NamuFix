1e84338df4d8f9fe1a8285f7013c793c6e0e9bc7
LiteHell <asdf1234d@gmail.com>
2015-05-29 19:13:41 +0900

    Location Detector Code Changed

--- src/CheckLocation.js
+++ src/CheckLocation.js
@@ -1,3 +1,3 @@
-var wikiloc='';
+var isEditing=false;
 if(document.querySelector("textarea[name=content]")!=null&&(/https?:\/\/[^\.]*\.?namu\.wiki\/edit.*/).test(location.href))
-wikiloc="edit";
+isEditing=true;
