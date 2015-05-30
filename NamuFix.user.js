710d0ebc08790590a220bdb304874c0bcd13a766
LiteHell <asdf1234d@gmail.com>
2015-05-30 23:52:06 +0900

    Fixed a bug and added some test code

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -112,5 +112,9 @@ if (IsEditing()) {
   editorModifier.addButton('T', '테스트 버튼', function() {
     alert('TEST');
   })
+  editorModifier.addSpace();
+  editorModifier.addButton('1', '테스트2', function() {
+    editorModifier.setStatus("TEST!");
+  })
 
 }
--- src/Editor/EditorFeatures.js
+++ src/Editor/EditorFeatures.js
@@ -1,1 +1,3 @@
 editorModifier.addButton('T','테스트 버튼',function(){alert('TEST');})
+editorModifier.addSpace();
+editorModifier.addButton('1','테스트2',function(){editorModifier.setStatus("TEST!");})
