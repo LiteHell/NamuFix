fd4686702d90fc3234c1a9424ad131551357e328
Nekopoly <spinpo12@naver.com>
2015-05-31 20:12:31 +0900

    regex 수정

--- src/CheckLocation.js
+++ src/CheckLocation.js
@@ -2,5 +2,5 @@ function IsEditing(){
   return document.querySelector("textarea[name=content]")!==null&&(/https?:\/\/[^\.]*\.?namu\.wiki\/edit.*/).test(location.href);
 }
 function IsWikipage() {
-  return (/https?:\/\/[^\.]*\.?namu\.wiki\/edit.*/).test(location.href)==true;
+  return (/https?:\/\/[^\.]*\.?namu\.wiki\/w\.*/).test(location.href)==true;
 }
