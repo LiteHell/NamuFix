f268fbe060252ae4a2dc92d92ecefb70eb8b3429
LiteHell <asdf1234d@gmail.com>
2015-08-10 20:23:06 +0900

    버전 올림

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -4,7 +4,7 @@
 // @description 나무위키 편집 인터페이스 등을 개선합니다.
 // @include     http://namu.wiki/*
 // @include     https://namu.wiki/*
-// @version     150810.3
+// @version     150810.4
 // @namespace   http://litehell.info/
 // @downloadURL https://raw.githubusercontent.com/LiteHell/NamuFix/master/NamuFix.user.js
 // @require     https://raw.githubusercontent.com/LiteHell/NamuFix/master/FlexiColorPicker.js
