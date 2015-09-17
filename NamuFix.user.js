c10c830c1a521072a13289bd6e91485958314044
LiteHell <asdf1234d@gmail.com>
2015-09-17 16:18:07 +0900

    버전 올림

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -6,7 +6,7 @@
 // @include     http://namu.wiki/*
 // @include     https://namu.wiki/*
 // @include     http://issue.namu.wiki/*
-// @version     150910.0
+// @version     150917.0
 // @namespace   http://litehell.info/
 // @downloadURL https://raw.githubusercontent.com/LiteHell/NamuFix/master/NamuFix.user.js
 // @require     https://raw.githubusercontent.com/LiteHell/NamuFix/master/FlexiColorPicker.js
