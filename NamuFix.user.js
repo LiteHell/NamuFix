0c02d79c035ec15ca24117181338b3af997efaa8
LiteHell <LiteHell@openmailbox.org>
2015-10-03 12:32:15 +0900

    버전 올림
    버전을 150929.2에서 151003.0으로 올림.

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -6,7 +6,7 @@
 // @include     http://namu.wiki/*
 // @include     https://namu.wiki/*
 // @include     http://issue.namu.wiki/*
-// @version     150929.2
+// @version     151003.0
 // @namespace   http://litehell.info/
 // @downloadURL https://raw.githubusercontent.com/LiteHell/NamuFix/master/NamuFix.user.js
 // @require     https://raw.githubusercontent.com/LiteHell/NamuFix/master/FlexiColorPicker.js
