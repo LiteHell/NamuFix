1c0cba3bc0dd6bcebac16af5a47f0cb7445f3cd0
LiteHell <LiteHell@openmailbox.org>
2015-10-08 22:47:24 +0900

    Increased version

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -6,7 +6,7 @@
 // @include     http://namu.wiki/*
 // @include     https://namu.wiki/*
 // @include     http://issue.namu.wiki/*
-// @version     151008.1
+// @version     151008.2
 // @namespace   http://litehell.info/
 // @downloadURL https://raw.githubusercontent.com/LiteHell/NamuFix/master/NamuFix.user.js
 // @require     https://raw.githubusercontent.com/LiteHell/NamuFix/master/FlexiColorPicker.js
