ca110ace65e4b5b8ef0b20db608167a0900ea49d
LiteHell <LiteHell@protonmail.com>
2017-10-01 03:46:47 +0900

    Add KISA WHOIS

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -24,13 +24,13 @@
 // @connect     cdn.rawgit.com
 // @connect     cdnjs.cloudflare.com
 // @connect     api.github.com
-// @connect     api.ipify.org
 // @connect     ipinfo.io
 // @connect     wtfismyip.com
 // @connect     www.googleapis.com
 // @connect     web.archive.org
 // @connect     archive.is
 // @connect     www.vpngate.net
+// @connect     namufix.wikimasonry.org
 // @grant       GM_addStyle
 // @grant       GM_openInTab
 // @grant       GM_xmlhttpRequest
@@ -213,6 +213,157 @@ function getIpInfo(ip, cb) {
     }
   });
 }
+
+var whoisDictionary = {};
+
+function getIpWhois(ip, cb) {
+  if (whoisDictionary[ip])
+    return cb(whoisDictionary[ip]);
+  GM_xmlhttpRequest({
+    method: "GET",
+    url: "http://namufix.wikimasonry.org/whois/ip/{0}".format(ip),
+    onload: function (res) {
+      var resObj = JSON.parse(res.responseText);
+      whoisDictionary[ip] = resObj;
+      cb(resObj);
+    }
+  });
+}
+
+function whoisPopup(ip) {
+  var win = TooSimplePopup();
+  win.title('IP WHOIS 조회');
+  win.button('다른 IP 조회', function () {
+    var newip = prompt('IP 주소를 입력하세요.');
+    win.close();
+    whoisPopup(newip);
+  })
+  win.button('닫기', win.close);
+  win.content(function (container) {
+    container.innerHTML = '조회중입니다. 잠시만 기다려주세요...';
+    getIpWhois(ip, function (result) {
+      if (result.success) {
+        var whoisObj = result.result;
+        var currentView = 'table';
+        container.innerHTML = '<p>NamuFix 서버를 통해 KISA WHOIS를 조회했습니다. 결과는 다음과 같습니다.(<a href="#" class="switchViewType">JSON 형식으로 보기</a>)</p><div class="whois-content"><table></table></div>';
+        var resultContainer = container.querySelector('.whois-content');
+        var switchViewLink = container.querySelector('a.switchViewType');
+
+        function useTableView() {
+          function writeSubInfoTable(subInfoObj, caption) {
+            var table = document.createElement('table');
+            table.className = "whois";
+            table.innerHTML += '<caption>' + encodeHTMLComponent(caption) + '</caption>';
+            table.innerHTML += '<thead><tr><th>이름</th><th>내용</th></tr></thead>';
+            var tableHtml = '<tbody>';
+            var koreanContactNames = {
+              techContact: '네트워크 담당자 정보',
+              adminContact: 'IP주소 관리자 정보',
+              abuseContact: '네트워크 abuse 담당자 정보'
+            };
+            var koreanNetInfoNames = {
+              // 주소범위(range), 프리픽스(prefix), 네트워크이름(servName), 기관명(orgname), 주소(addr), 우편번호(zipCode), 할당내역 등록일(regDate) 필드 포함
+              addr: '주소',
+              netType: '네트워크 구분',
+              orgName: '기관명',
+              orgID: '기관 ID',
+              prefix: 'IP prefix',
+              range: 'IP 범위',
+              regDate: '등록일',
+              servName: '네트워크 이름',
+              zipCode: '우편번호'
+            };
+            for (var i in subInfoObj) {
+              if (i == 'techContact' || i == 'adminContact' || i == 'abuseContact') {
+                tableHtml += '<tr><td colspan="2" class="whois-subheader">' + koreanContactNames[i] + '</td></tr>';
+                if (subInfoObj[i].name)
+                  tableHtml += '<tr><td>이름</td><td>' + encodeHTMLComponent(subInfoObj[i].name) + '</td></tr>';
+                if (subInfoObj[i].phone)
+                  tableHtml += '<tr><td>전화번호</td><td>' + encodeHTMLComponent(subInfoObj[i].phone) + '</td></tr>';
+                if (subInfoObj[i].email)
+                  tableHtml += '<tr><td>연락처</td><td>' + encodeHTMLComponent(subInfoObj[i].email) + '</td></tr>';
+              } else if (i == 'netinfo') {
+                // KISA 씨발놈들.... API 설명서에 다 적어두지...
+                tableHtml += '<tr><td colspan="2" class="whois-subheader">네트워크 정보</td></tr>';
+                for (var na in subInfoObj[i]) {
+                  tableHtml += '<tr><td>' + (koreanNetInfoNames[na] ? koreanNetInfoNames[na] : na) + '</td><td>' + encodeHTMLComponent(subInfoObj[i][na]) + '</td></tr>';
+                }
+              }
+            }
+            tableHtml += '</tbody>';
+            table.innerHTML += tableHtml;
+            return table;
+          }
+          switchViewLink.innerText = '(진행중입니다)';
+          currentView = 'progress';
+          resultContainer.innerHTML = '<p>(진행중입니다.)</p>'
+
+          function writeRest() {
+            if (whoisObj.korean && whoisObj.korean.ISP)
+              resultContainer.appendChild(writeSubInfoTable(whoisObj.korean.ISP, "IP 주소 보유기관 정보 (국문)"));
+            if (whoisObj.korean && whoisObj.korean.user)
+              resultContainer.appendChild(writeSubInfoTable(whoisObj.korean.user, "IP 주소 이용기관 정보 (국문)"));
+            if (whoisObj.english && whoisObj.english.ISP)
+              resultContainer.appendChild(writeSubInfoTable(whoisObj.english.ISP, "IP 주소 보유기관 정보 (영문)"));
+            if (whoisObj.english && whoisObj.english.user)
+              resultContainer.appendChild(writeSubInfoTable(whoisObj.english.user, "IP 주소 이용기관 정보 (영문)"));
+            switchViewLink.innerText = 'JSON 형식으로 보기';
+            currentView = 'table';
+          }
+          if (whoisObj.countryCode == 'none') {
+            resultContainer.innerHTML = '<style>.whois-subheader {background: #9D75D9; color: white; text-align: center;} table.whois tr, table.whois td{border: 1px #9D75D9 solid; border-collapse: collapse;} table.whois td {padding: 5px;} table.whois caption { caption-side: top; }</style>' +
+              '<table class="whois"><caption>쿼리 정보</caption>' +
+              '<thead><th>이름</th><th>내용</th></tr></thead>' +
+              '<tbody>' +
+              '<tr><td>쿼리</td><td>' + whoisObj.query + '</td></tr>' +
+              '<tr><td>쿼리 유형</td><td>' + whoisObj.queryType + '</td></tr>' +
+              '<tr><td>레지스트리</td><td>' + whoisObj.registry + '</td></tr>' +
+              '<tr><td>등록 국가</td><td><span class="icon ion-android-star"></span> 없음 (루프백같이 특수한 경우)</td></tr>' +
+              '</tbody></table>';
+            writeRest();
+          } else {
+            getFlagIcon(whoisObj.countryCode.toLowerCase(), function (flagIconData) {
+              resultContainer.innerHTML = '<style>.whois-subheader {background: #9D75D9; color: white; text-align: center;} table.whois tr, table.whois td{border: 1px #9D75D9 solid; border-collapse: collapse;} table.whois td {padding: 5px;} table.whois caption { caption-side: top; }</style>' +
+                '<table class="whois"><caption>쿼리 정보</caption>' +
+                '<thead><th>이름</th><th>내용</th></tr></thead>' +
+                '<tbody>' +
+                '<tr><td>쿼리</td><td>' + whoisObj.query + '</td></tr>' +
+                '<tr><td>쿼리 유형</td><td>' + whoisObj.queryType + '</td></tr>' +
+                '<tr><td>레지스트리</td><td>' + whoisObj.registry + '</td></tr>' +
+                '<tr><td>등록 국가</td><td><img style="height: 1em;" src="' + flagIconData + '">' + korCountryNames[whoisObj.countryCode.toUpperCase()] + '</td></tr>' +
+                '</tbody></table>';
+              writeRest();
+            });
+          }
+        }
+
+        function useJsonView() {
+          switchViewLink.innerText = '표 형식으로 보기';
+          currentView = 'json';
+          resultContainer.innerHTML = '<textarea readonly style="width: 50vw; height: 600px; max-height: 90vh;"></textarea>';
+          resultContainer.querySelector('textarea').value = JSON.stringify(whoisObj);
+        }
+        switchViewLink.addEventListener('click', function (evt) {
+          evt.preventDefault();
+          if (currentView == 'json') {
+            useTableView();
+          } else if (currentView == 'table') {
+            useJsonView();
+          } else if (currentView == 'progress') {
+            alert('잠시만 기다려주세요.');
+          }
+        })
+        useTableView();
+      } else if (result.error.namufix) {
+        alert('NamuFix 서버측에서 오류가 발생했습니다.\n\n메세지 : ' + result.error.message + '\n오류 코드 : ' + result.error.code);
+        win.close();
+      } else if (result.error.kisa) {
+        container.innerHTML = '<p>KISA WHOIS 조회결과 다음과 같은 오류가 반환됐습니다.</p><p><em>' + result.error.message + '</em></p><p>오류 코드는 ' + result.error.code + '입니다.</p>';
+      }
+    });
+  });
+}
+
 // To bypass CSP
 var flagIconDictionary = {};
 
@@ -1973,11 +2124,19 @@ function mainFunc() {
               console.log(countryName);
               console.log(isp);
               getFlagIcon(country.toLowerCase(), function (data) {
-                span.innerHTML = '[<img src="{0}" style="height: 0.9rem;" title="{3}"></img> {1}{2}]'.format(data, isp, vpngateIP.indexOf(ip) != -1 ? " (VPNGATE)" : "", countryName);
+                span.innerHTML = '[<img src="{0}" style="height: 0.9rem;" title="{3}"></img> {1}{2}]<a href="#" class="get-whois">[WHOIS]</a>'.format(data, isp, vpngateIP.indexOf(ip) != -1 ? " (VPNGATE)" : "", countryName);
+                span.querySelector('a.get-whois').addEventListener('click', function (evt) {
+                  evt.preventDefault();
+                  whoisPopup(ip);
+                });
                 checkIP(vpngateIP);
               });
             } else {
-              span.innerHTML = "[IP조회실패]"
+              span.innerHTML = "[IP조회실패]<a href=\"#\" class=\"get-whois\">[WHOIS]</a>"
+              span.querySelector('a.get-whois').addEventListener('click', function (evt) {
+                evt.preventDefault();
+                whoisPopup(ip);
+              })
               checkIP(vpngateIP);
             }
           });
@@ -2100,12 +2259,17 @@ function mainFunc() {
               "<tr><td>국가</td><td><img src=\"{0}\" style=\"height: 0.9rem;\"></img> {1}</td></tr>" + // {0} : cotunry, {1} : countryName
               "<tr><td>통신사</td><td>{2}</td></tr>" +
               "<tr><td>VPNGATE?</td><td>{3}</td></tr>" +
+              '<tr><td>KISA WHOIS</td><td><a href="#" class="get-whois">조회하기</a></td></tr>' +
               "</tbody>" +
               '<tfoot>' +
               '<tr><td colspan="2" style="border-top: 1px solid black;">기술적인 한계로, VPNGATE 여부는 "현재 VPNGATE VPN인가?"의 여부이지, "작성 당시에 VPNGATE VPN인가?"의 여부가 아닙니다.</td></tr>' +
               '</foot>' +
               "</table>"
             ).format(countryIcon, countryName, isp, result.indexOf(ip) != -1 ? "<span style=\"color: red;\">YES! This is currently WORKING VPNGATE IP!</span>" : "Not a vpngate ip");
+            ipInfo.querySelector('a.get-whois').addEventListener('click', function (evt) {
+              evt.preventDefault();
+              whoisPopup(ip);
+            })
           });
         });
       });
@@ -2409,6 +2573,11 @@ addItemToMemberMenu('설정 백업/복원', function (evt) {
   } else {
     alert('취소됨.');
   }
+});
+addItemToMemberMenu('KISA WHOIS', function (evt) {
+  evt.preventDefault();
+
+  whoisPopup(prompt('조회할 IP주소를 입력하세요.'));
 })
 mainFunc();
 listenPJAX(mainFunc);
