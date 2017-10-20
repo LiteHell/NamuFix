54ac447cf12a644b2180e15486e188c9c1f7bdbc
LiteHell <LiteHell@protonmail.com>
2017-10-21 01:14:42 +0900

    fixed a bug

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -1234,6 +1234,7 @@ function mainFunc() {
                   }
                 });
               }
+              sendUploadReq();
             })
           }
           if (present_files != null) {
