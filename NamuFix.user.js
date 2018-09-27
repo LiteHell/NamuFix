b6d115be6ab69c70a1876664d9afc73f7a80924a
LiteHell <litehell@litehell.info>
2018-09-27 18:20:38 +0900

    Remove alphawiki support
     * alphawiki doesn't use the seed engine.

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -3,8 +3,6 @@
 // @namespace   http://litehell.info/
 // @description 나무위키 등 더시드 사용 위키의 편집 인터페이스 등을 개선합니다.
 // @include     https://namu.wiki/*
-// @include     https://alphawiki.org/*
-// @include     https://www.alphawiki.org/*
 // @include     https://theseed.io/*
 // @include     https://board.namu.wiki/*
 // @version     180923.0
