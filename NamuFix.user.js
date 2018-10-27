f292815c8c9be50da629f4a7387bf4e699998c6a
LiteHell <litehell@litehell.info>
2018-10-27 12:31:10 +0900

    Increased version
     * 181027.0

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -5,7 +5,7 @@
 // @include     https://namu.wiki/*
 // @include     https://theseed.io/*
 // @include     https://board.namu.wiki/*
-// @version     180923.0
+// @version     181027.0
 // @author      LiteHell
 // @downloadURL https://namufix.wikimasonry.org/latest.js
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
