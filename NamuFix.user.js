2e4e399ebd6349a37710e1abceb81d5e02522c86
LiteHell <litehell@litehell.info>
2019-07-27 02:22:44 +0900

    Added deleted code due to mistake

--- NamuFix.user.js
+++ NamuFix.user.js
@@ -3587,6 +3587,31 @@ if (location.host === 'board.namu.wiki') {
                });
             }
          }
+      } else if (ENV.IsRecentChanges) {
+         let changeRows = document.querySelectorAll('article table.table tbody tr');
+         for (let row of changeRows) {
+            if (!row.querySelector('a')) continue; // 편집 코멘트 필요없음.
+            let author = {
+               name: row.querySelector('td:nth-child(2) a')
+                  .textContent.trim()
+            }
+            author.isIP = validateIP(author.name);
+            if (SET.addQuickBlockLink) {
+               let quickBlockAnchor = document.createElement("a");
+               quickBlockAnchor.textContent = " [차단] "
+               quickBlockAnchor.href = "#";
+               quickBlockAnchor.addEventListener('click', (evt) => {
+                  evt.preventDefault();
+                  quickBlockPopup({
+                     author: author,
+                     defaultReason: '긴급차단 - 문서 훼손',
+                     defaultDuration: SET.quickBlockDefaultDuration
+                  });
+               })
+               row.querySelector('td:first-child')
+                  .insertBefore(quickBlockAnchor, row.querySelector('td:first-child span'));
+            }
+         }
       }
       if (ENV.IsEditRequest) {
          if (SET.addQuickBlockLink) {
